0578e861e43b96a87447359a66008d4a062ae916
mparamoreyes <35793192+mparamoreyes@users.noreply.github.com>
2018-01-26 19:24:35 -0600

    Add files via upload

---
+++ HelloWorldName.c
@@ -0,0 +1,14 @@
+/**
+Author: Maricela Paramo 
+Date: 01/26/2018
+This program prints my name.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argb) 
+{
+printf("Maricela Paramo!\n");
+
+return 0;
+}
